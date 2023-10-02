f4c4616c99be86b4a068bb7b6ea20aa8699dba46
Steven Fraga <sfraga1000@gmail.com>
2023-10-01 21:34:59 -0700

    CIS-12 Assignment 5 Complete Steven Fraga

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -23,6 +23,9 @@ public class TempConversion {
         return kelvin - 273.15;
     }
 
+    public static double convertK2F(double kelvin) { return convertC2F(convertK2C(kelvin)); }
+
+
     public static double getTemp(String unit) {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
@@ -35,10 +38,48 @@ public class TempConversion {
         System.out.printf("%-4s : %40s%n", "Q).", "To quit");
         return input.nextLine();
     }
-
     public static void main(String[] args) {
-        while(true) {
+        String from = getUnitChoice();
+        if (from.equals("Q")) {
+            System.out.println("Have a nice day.");
+            return;
+        }
+        String to = getUnitChoice();
+        if (to.equals("Q")) {
+            System.out.println("Have a nice day.");
+            return;
+        }
+        double x = getTemp(from);
+        String caseStr = from + to;
+        while (true) {
+            switch (caseStr) {
+                case "CF" -> {
+                    System.out.printf("Result %f°C is %f°F\n", x, convertC2F(x));
+                    return;
+                }
+                case "CK" -> {
+                    System.out.printf("Result %f°C is %f°K\n", x, convertC2K(x));
+                    return;
+                }
+                case "FC" -> {
+                    System.out.printf("Result %f°F is %f°C\n", x, convertF2C(x));
+                    return;
+                }
+                case "FK" -> {
+                    System.out.printf("Result %f°F is %f°K\n", x, convertF2K(x));
+                    return;
+                }
+                case "KC" -> {
+                    System.out.printf("Result %f°K is %f°C\n", x, convertK2C(x));
+                    return;
+                }
+                case "KF" -> {
+                    System.out.printf("Result %f°K is %f°F\n", x, convertK2F(x));
+                    return;
+                }
+                default -> System.out.println("No Match");
 
+            }
         }
     }
 }
