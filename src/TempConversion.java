3434db6dc54a2b09e704103f8355a49922f1aa5f
jojov <josephverdin55@gmail.com>
2023-10-22 22:44:04 -0700

    Finished Assignment 005 ran into error not sure whats wrong Joseph Verdin

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -23,6 +23,8 @@ public class TempConversion {
         return kelvin - 273.15;
     }
 
+    public static double convertK2F(double kelvin) { return (kelvin - 273.15) * 1.8 + 32; }
+
     public static double getTemp(String unit) {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
@@ -38,7 +40,63 @@ public class TempConversion {
 
     public static void main(String[] args) {
         while(true) {
+            System.out.println("Unit Converting from: ");
+            String unFrom = getUnitChoice();
 
+            if(unFrom.equalsIgnoreCase("Q")) {
+                System.out.println("Quitting Program...");
+                break;
+            }
+
+            System.out.println("Unit Converting To: ");
+            String unTo = getUnitChoice();
+
+            if (unTo.equalsIgnoreCase("Q")) {
+                System.out.println("Quitting Program...");
+                break;
+            }
+
+            System.out.println("Temperature Value: ");
+            double tempV = getTemp(unFrom);
+
+            double result;
+            switch (unFrom.toUpperCase()) {
+                case "C":
+                    switch (unTo.toUpperCase()) {
+                        case "F":
+                            result = convertC2F(tempV);
+                            System.out.printf("Result %.6f°C is %.6f°F%n", tempV, result);
+                            break;
+                        case "K":
+                            result = convertC2K(tempV);
+                            System.out.printf("Result %.6f°C is %.6f°K%n", tempV, result);
+                            break;
+                    }
+                    break;
+                case "F":
+                    switch (unTo.toUpperCase()) {
+                        case "C":
+                            result = convertF2C(tempV);
+                            System.out.printf("Result %.6f°F is %.6f°C%n", tempV, result);
+                            break;
+                        case "K":
+                            result = convertF2K(tempV);
+                            System.out.printf("Result %.6f°F is %.6f°K%n", tempV, result);
+                            break;
+                    }
+                    break;
+                case "K":
+                    switch (unTo.toUpperCase()) {
+                        case "C":
+                            result = convertK2C(tempV);
+                            System.out.printf("Result %.6f°K is %.6f°C%n", tempV, result);
+                            break;
+                        case "F":
+                            result = convertK2F(tempV);
+                            System.out.printf("Result %.6f°K is %.6f°F%n", tempV, result);
+                            break;
+                    }
+                    break;
         }
     }
 }
